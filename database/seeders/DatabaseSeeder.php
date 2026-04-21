0ef3f869a70e363e82e9a0b761ad3b4f6624596d
hadrianfdc <hadrian.fdc@gmail.com>
2026-04-21 15:42:24 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PlanSeeder::class,
             ThemeSeeder::class,
+            SuperAdminSeeder::class
         ]);
     }
 }
